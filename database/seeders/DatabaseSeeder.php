342595d1892cc8ce88223b4b99949d35f4930908
anasseELQOUR <anasseelqour1996@gmail.com>
2022-04-01 00:10:30 +0000

    Instatllation livewire + creation des migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Client;
 use Illuminate\Database\Seeder;
+use Prophecy\Call\Call;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(TypeArticleTableSeeder::class);
         // \App\Models\User::factory(10)->create();
+        Article::factory(100)->create();
+        Client::factory(100)->create();
+
+        $this->call(RoleTableSeeder::class);
+        $this->call(PermissionTableSeeder::class);
+        $this->call(StatutLocationTableSeeder::class);
+        $this->call(DureeLocationTableSeeder::class);
+
     }
 }
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div style="text-align: center">
+    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+</div>
